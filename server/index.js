8f2b77fd798b645edcceebc6cf06d22af1f12f1e
João <jomaatheus@gmail.com>
2025-03-14 14:20:18 -0300

    adiciona tratamento de promessa para sincronização do sequelize

--- server/index.js
+++ server/index.js
@@ -90,8 +90,8 @@ app.use('/graphql', graphqlHTTP({
 }));
 
 setupAssociations(); 
-
 sequelize.sync({ alter: true }) 
+  .then(() => {
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
       console.log(`Swagger docs available at http://localhost:${port}/api-docs`);
